ccead9cb5d6c2c6213d00585756b20aab44e34e5
Emanuel <65658910+Many98@users.noreply.github.com>
2024-05-02 14:54:26 +0200

    revised RuianFetcher api

--- ruian.py
+++ ruian.py
@@ -22,12 +22,60 @@ class RuianFetcher(Connector):
 
     def __init__(self) -> None:
         self.__data = None
+
         self.address_formatter = AddressFormatter(RemoveElementsFromLeftStrategy())
 
 
+    @staticmethod
+    def code_api_details(address: str) -> Tuple:
+        """Provide api details for ruian code API
+
+        Returns:
+            Tuple: (url: str, params: dict, headers: dict)
+        """
+
+        return ("https://vdp.cuzk.cz/vdp/ruian/adresnimista/fulltext", \
+                
+                {'adresa': address}, \
+
+                {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+                    "Accept-Encoding": "gzip, deflate, br, zstd",
+                    "Accept-Language": "en-GB,en;q=0.6",
+                    "Cache-Control": "max-age=0",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "none",
+                    "Sec-Fetch-User": "?1",
+                    "Sec-Gpc": "1",
+                    "Upgrade-Insecure-Requests": "1",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+                }
+        )
+    
+    @staticmethod
+    def coor_api_details(address: str) -> Tuple:
+        """Provide api details for coordinates API
+
+        Returns:
+            Tuple: (url: str, params: dict, headers: dict)
+        """
+
+        return ("https://ags.cuzk.cz/arcgis/rest/services/RUIAN/Vyhledavaci_sluzba_nad_daty_RUIAN/MapServer/exts/GeocodeSOE/findAddressCandidates", \
+                
+                {
+            'SingleLine': address,
+            'f': 'pjson'
+        }, \
+
+                {
+                    
+                }
+        )
+
     def ensure_length_limit(self, address: str) -> str:
         """
-        make sure address is shorther than 40 chars
+        Makes sure address is shorther than 40 chars (required by code API)
         """
         if len(address) > 40:
             address = self.address_formatter.cleanse(address)
@@ -37,32 +85,14 @@ class RuianFetcher(Connector):
 
     def fetch_ruian_code(self, address: str) -> RuianCodeApiResponse:
         """
-        Obtains ruian code given address
+        Fetch RUIAN code data from RUIAN API for given address
         """
 
         # TODO prepare for 3 situations, error, success and success without found match
 
         address = self.ensure_length_limit(address)  # TODO not sure if here is good place for applying this method
 
-        url = "https://vdp.cuzk.cz/vdp/ruian/adresnimista/fulltext"
-
-        params = {
-            'adresa': address
-        }
-
-        headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "en-GB,en;q=0.6",
-            "Cache-Control": "max-age=0",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "Sec-Gpc": "1",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-        }
+        url, params, headers = self.code_api_details(address)
 
         response = requests.get(url, headers=headers, params=params)
 
@@ -77,59 +107,13 @@ class RuianFetcher(Connector):
             
         except Exception as e:
             return RuianCodeApiResponse({"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"{str(e)}"})
-
-
-    async def afetch_ruian_code(self, address: str) -> RuianCodeApiResponse:
-        """
-        asynchronous implementation of fetch ruian code
-        """
-        address = self.ensure_length_limit(address)  # TODO not sure if here is good place for applying this method
-
-        url = "https://vdp.cuzk.cz/vdp/ruian/adresnimista/fulltext"
-
-        params = {
-            'adresa': address
-        }
-
-        headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "en-GB,en;q=0.6",
-            "Cache-Control": "max-age=0",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "Sec-Gpc": "1",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-        }
-        async with asyncio.Semaphore(5):  # max 5 concurrent requests
-            async with aiohttp.ClientSession() as session:
-                try:
-                    async with session.get(url=url, headers=headers, params=params) as response:
-                        if response.status == 200:
-                            json_data = await response.json()
-                            print(json_data)
-                            api_response = RuianCodeApiResponse(**json_data)
-                            if not api_response.polozky:
-                                api_response.polozky = [None]
-                            return api_response
-                        else:
-                            return RuianCodeApiResponse(**{"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"HTTP Error {response.status}"})
-                except Exception as e:
-                    return RuianCodeApiResponse({"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"{str(e)}"})
             
     def fetch_coordinates(self, address: str) -> CoordinatesAPIResponse:
         """
-        obtains coordinates for given address
+        Fetch data about coordinates from RUIAN API for given address
         """
-        url = "https://ags.cuzk.cz/arcgis/rest/services/RUIAN/Vyhledavaci_sluzba_nad_daty_RUIAN/MapServer/exts/GeocodeSOE/findAddressCandidates"
 
-        params = {
-            'SingleLine': address,
-            'f': 'pjson'
-        }
+        url, params, _ = self.coor_api_details(address)
 
         response = requests.get(url, params=params)
 
@@ -144,19 +128,14 @@ class RuianFetcher(Connector):
                                            "candidates": [None],
                                            "error_msg": f"{str(e)}"
                                            })
-
-    async def afetch_coordinates(self, address: str) -> CoordinatesAPIResponse:
+        
+    def bulk_fetch_ruian_codes(self, column_name: str = 'undefined', file_path: str = '', server: str = '', db: str = '', in_table: str = '') -> List[RuianCodeApiResponse]:
         """
-        asynchronous implementation of fetch coordinates
+        Batch process multiple addresses
         """
-        pass
 
+        # TODo add export option as well
 
-
-    def run(self, column_name: str = 'undefined', file_path: str = '', server: str = '', db: str = '', in_table: str = '') -> None:
-        """
-        batch process multiple addresses
-        """
         data = self.load('auto', file_path, server, db, in_table)
 
         if column_name not in data:
@@ -186,13 +165,70 @@ class RuianFetcher(Connector):
         matches2 = [res.address for res in responses]
         """
 
-
         self.export(data, 'excel', 'ruian_processed')
+
+    def bulk_fetch_coordinates(self, column_name: str = 'undefined', file_path: str = '', server: str = '', db: str = '', in_table: str = '') -> List[CoordinatesAPIResponse]:
+        """
+        Batch process multiple addresses
+        """
+
+        # TODo add export option as well
+
+        data = self.load('auto', file_path, server, db, in_table)
+
+        pass
+        
+    async def afetch_ruian_code(self, address: str) -> RuianCodeApiResponse:
+        """
+        Asynchronous implementation of `fetch_ruian_code` method
+        """
+        address = self.ensure_length_limit(address)  # TODO not sure if here is good place for applying this method
+
+        url, params, headers = self.code_api_details(address)
+
+        async with asyncio.Semaphore(5):  # max 5 concurrent requests
+            async with aiohttp.ClientSession() as session:
+                try:
+                    async with session.get(url=url, headers=headers, params=params) as response:
+                        if response.status != 200:
+                            json_data = await response.json()
+                            
+                            api_response = RuianCodeApiResponse(**json_data)
+                            if not api_response.polozky:
+                                api_response.polozky = [None]
+                            return api_response
+                        else:
+                            return RuianCodeApiResponse(**{"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"HTTP Error {response.status}"})
+                except Exception as e:
+                    return RuianCodeApiResponse({"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"{str(e)}"})
+
+    async def afetch_coordinates(self, address: str) -> CoordinatesAPIResponse:
+        """
+        Asynchronous implementation of `fetch_coordinates` method
+        """
+        url, params, _ = self.coor_api_details(address)
+
+        async with asyncio.Semaphore(5):  # max 5 concurrent requests
+            async with aiohttp.ClientSession() as session:
+                try:
+                    async with session.get(url=url, params=params) as response:
+                        if response.status != 200:
+                            json_data = await response.json()
+                            api_response = RuianCodeApiResponse(**json_data)
+
+                            if not api_response.polozky:
+                                api_response.polozky = [None]
+                            return api_response
+                        else:
+                            return RuianCodeApiResponse(**{"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"HTTP Error {response.status}"})
+                except Exception as e:
+                    return RuianCodeApiResponse({"polozky":[None],"existujiDalsiPolozky": False, "error_msg": f"{str(e)}"})
+
         
 
-    async def arun(self, column_name: str = 'undefined', file_path: str = '', server: str = '', db: str = '', in_table: str = '') -> None:
+    async def abulk_fetch_ruian_codes(self, column_name: str = 'undefined', file_path: str = '', server: str = '', db: str = '', in_table: str = '') -> List[RuianCodeApiResponse]:
         """
-        asynchronously batch process multiple addresses
+        Asynchronously batch process multiple addresses
         """
         data = self.load('auto', file_path, server, db, in_table)
         
@@ -224,7 +260,11 @@ class RuianFetcher(Connector):
         ruians = [res.polozky[0].kod if res.polozky[0] is not None else None for res in responses]
         matches = [res.nazev if res is not None else None for res in responses]
 
-
+    async def abulk_fetch_coordinates(self, column_name: str = 'undefined', file_path: str = '', server: str = '', db: str = '', in_table: str = '') -> List[CoordinatesAPIResponse]:
+        """
+        Asynchronously batch process multiple addresses
+        """
+        data = self.load('auto', file_path, server, db, in_table)
 
 
 if __name__ == "__main__":
